49a6f69622fb425e32a1d29e99ab168bf8934674
Mitko Nikov <mitkonikov01@gmail.com>
2016-12-22 14:23:08 +0100

    improving the path finding algorithm

--- motor.cpp
+++ motor.cpp
@@ -4,52 +4,12 @@
 
 using namespace std;
 
-class Cell {
-private:
-    int cx, cy, cv;
-    bool cp, ck, visited;
-public:
-    Cell(int xi, int yi, bool pi, bool ki, int vi) {
-        cx = xi;
-        cy = yi;
-        cp = pi;
-        ck = ki;
-        cv = vi;
-        visited = false;
-    }
-
-    int getX() {
-        return cx;
-    }
-    int getY() {
-        return cy;
-    }
-    int getV() {
-        return cv;
-    }
-    bool getP() {
-        return cp;
-    }
-    bool getK() {
-        return ck;
-    }
-    bool isnotVisited() {
-        return !visited;
-    }
-
-    void setV(int input) {
-        cv = input;
-    }
-    void setVisited() {
-        visited = true;
-    }
-};
-
-vector<  vector<Cell>  > matrix;
+vector<  vector<int>  > matrix;
+bool globalFOUND = false;
+int best = 0;
 int sy, sx;
 int py, px;
-int ky, kx;
-bool globalFOUND = false;
+int counter = 1;
 
 void printArray() {
     cout << endl;
@@ -58,7 +18,7 @@ void printArray() {
     cout << "Printing Array :" << endl;
     for (int i = 0; i < sy; ++i) {
         for (int ii = 0; ii < sx; ++ii) {
-            cout << matrix[i][ii].getV() << " ";
+            cout << matrix[i][ii] << " ";
         }
         cout << endl;
     }
@@ -66,65 +26,77 @@ void printArray() {
 }
 
 bool testCheckK(int x, int y) {
-    if (matrix[y][x + 1].getK()) // Right
+    if (matrix[y][x + 1] == -5) // Right
         return true;
 
-    if (matrix[y + 1][x].getK()) // Down
+    if (matrix[y + 1][x] == -5) // Down
         return true;
 
-    if (matrix[y][x - 1].getK()) // Left
+    if (matrix[y][x - 1] == -5) // Left
         return true;
 
-    if (matrix[y - 1][x].getK()) // Up
+    if (matrix[y - 1][x] == -5) // Up
         return true;
 
     return false;
 }
 
-int rec(int x, int y) {
+void setting(int x, int y, int input) {
+    if (matrix[y][x + 1] == 0) { // Right
+        matrix[y][x + 1] = input;
+    }
+
+    if (matrix[y + 1][x] == 0) { // Down
+        matrix[y + 1][x] = input;
+    }
+
+    if (matrix[y][x - 1] == 0) { // Left
+        matrix[y][x - 1] = input;
+    }
+
+    if (matrix[y - 1][x] == 0) { // Up
+        matrix[y - 1][x] = input;
+    }
+}
+
+void setSurroundingP(int x, int y) {
+    printArray();
+    if (testCheckK(x, y)) { // Proveri dali nekoj od sosedite e K
+        globalFOUND = true;
+        best = 1;
+    }
+    setting(x, y, 1);
+}
+
+void setSurrounding(int x, int y) {
     if (!globalFOUND) {
-        matrix[y][x].setVisited();
         printArray();
         if (testCheckK(x, y)) { // Proveri dali nekoj od sosedite e K
             globalFOUND = true;
-            return matrix[y][x].getV() + 1;
+            best = matrix[y][x] + 1;
         }
-
-        if (matrix[y][x + 1].isnotVisited() && matrix[y][x + 1].getV() == 0) { // Right
-            matrix[y][x + 1].setV(matrix[y][x].getV() + 1);
-            return rec(x + 1, y);
-        }
-
-        if (matrix[y + 1][x].isnotVisited() && matrix[y + 1][x].getV() == 0) { // Down
-            matrix[y + 1][x].setV(matrix[y][x].getV() + 1);
-            return rec(x, y + 1);
-        }
-
-        if (matrix[y][x - 1].isnotVisited() && matrix[y][x - 1].getK() == 0) { // Left
-            matrix[y][x - 1].setV(matrix[y][x].getV() + 1);
-            return rec(x - 1, y);
-        }
-
-        if (matrix[y - 1][x].isnotVisited() && matrix[y - 1][x].getK() == 0) { // Up
-            matrix[y - 1][x].setV(matrix[y][x].getV() + 1);
-            return rec(x, y - 1);
-        }
-    } else
-        return -1;
+        setting(x, y, matrix[y][x] + 1);
+    }
 }
 
-int calc() {
+void calc() {
     printArray();
 
-    // Go bara P
-    for (int i = 0; i < sy; ++i) {
-        for (int ii = 0; ii < sx; ++ii) {
-            if (matrix[i][ii].getP()) // Proveri dali e P
-                return rec(ii, i);
+    while (true) {
+        for (int i = 0; i < sy; ++i) {
+            for (int ii = 0; ii < sx; ++ii) {
+                if (matrix[i][ii] == -3) // Proveri dali e P
+                    setSurroundingP(ii, i);
+                else if (matrix[i][ii] == counter)
+                    setSurrounding(ii, i);
+            }
         }
-    }
 
-    return -1;
+        if (best > 0)
+            break;
+
+        counter++;
+    }
 }
 
 int main()
@@ -145,32 +117,25 @@ int main()
 
     // Ja pretvara matricata vo brojki za polesna obrabotka
     for (int y = 0; y < sy; ++y) {
-        vector<Cell> temp;
+        vector<int> temp;
         for (int x = 0; x < sx; ++x) {
             if (cont[y][x] == '.') {
-                temp.push_back(Cell(x, y, false, false, 0)); // MOZE
-            } else if (cont[y][x] == 'x') {
-                temp.push_back(Cell(x, y, false, false, -1)); // ZID
+                temp.push_back(0); // MOZE
+            } else if (cont[y][x] == '+') {
+                temp.push_back(-1); // ZID
             } else if (cont[y][x] == 'P') {
-                temp.push_back(Cell(x, y, true, false, 0)); // POCETOk
+                temp.push_back(-3); // POCETOk
             } else if (cont[y][x] == 'K') {
-                temp.push_back(Cell(x, y, false, true, 0)); // KRAJ
+                temp.push_back(-5); // KRAJ
             } else {
-                temp.push_back(Cell(x, y, false, false, -1));
+                temp.push_back(-1);
             }
         }
         matrix.push_back(temp);
     }
 
-    int c = calc();
-    cout << endl;
-    cout << "-------------------" << endl;
-    cout << endl;
-    if (c == -1) {
-        cout << "FAILED!" << endl;
-    } else {
-        cout << c << endl;
-    }
+    calc();
+    cout << "\n-------------------\n\n" << best << endl;
 
     return 0;
 }
